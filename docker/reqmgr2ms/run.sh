85dd91884735a94ca0e46b26478b088efbf48bd7
Kenyi Hurtado <khurtado@nd.edu>
2020-06-17 15:39:06 -0400

    Update run.sh

--- docker/reqmgr2ms/run.sh
+++ docker/reqmgr2ms/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 srv=`echo $USER | sed -e "s,_,,g"`
 
+echo Line 1 > /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
+
 # overwrite host PEM files in /data/srv area
 if [ -f /etc/secrets/robotkey.pem ]; then
     sudo cp /etc/secrets/robotkey.pem /data/srv/current/auth/$srv/dmwm-service-key.pem
@@ -23,6 +28,10 @@ if [ -f /etc/proxy/proxy ]; then
     fi
     ln -s /etc/proxy/proxy /data/srv/current/auth/proxy/proxy
 fi
+echo Line 2 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 # overwrite header-auth key file with one from secrets
 if [ -f /etc/secrets/hmac ]; then
@@ -39,6 +48,10 @@ if [ -f /etc/secrets/hmac ]; then
     sudo cp /etc/secrets/hmac /data/srv/current/auth/$srv/header-auth-key
     sudo chown $USER.$USER /data/srv/current/auth/$srv/header-auth-key
 fi
+echo Line 3 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 # In case there is at least one configuration file under /etc/secrets, remove
 # the default config files from the image and copy over those from the secrets area
@@ -52,12 +65,23 @@ for fname in $cfiles; do
         break
     fi
 done
+
+echo Line 4 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
+
 for fname in $cfiles; do
     if [ -f /etc/secrets/$fname ]; then
         sudo cp /etc/secrets/$fname $cdir/$fname
         sudo chown $USER.$USER $cdir/$fname
     fi
 done
+
+echo Line 5 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 files=`ls $cdir`
 for fname in $files; do
@@ -70,6 +94,11 @@ for fname in $files; do
     fi
 done
 
+echo Line 6 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
+
 files=`ls /etc/secrets`
 for fname in $files; do
     if [ ! -f $cdir/$fname ]; then
@@ -77,6 +106,10 @@ for fname in $files; do
         sudo chown $USER.$USER /data/srv/current/auth/$srv/$fname
     fi
 done
+echo Line 7 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 # Sleep for a while before starting services
 sleep 2
@@ -86,15 +119,35 @@ ls -l $cdir > /tmp/debug 2>&1
 # start the service
 # if it is ms-output, then we also need to start mongodb
 if [ -f $cdir/config-output.py ]; then
-    echo "I got in this if" > /tmp/debug
+    echo "I got in this if" >> /tmp/debug
     /data/srv/current/config/mongodb/manage start 'I did read documentation'
 fi
+echo Line 8 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
+
+echo /data/srv/current/config/$srv/manage start 'I did read documentation' >> /tmp/debug
 /data/srv/current/config/$srv/manage start 'I did read documentation'
+
+echo Line 9 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 # run monitoring script
 if [ -f /data/monitor.sh ]; then
     /data/monitor.sh
 fi
+echo Line 10 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
 
 # start cron daemon
 sudo /usr/sbin/crond -n
+
+echo Line 11 >> /tmp/debug
+date >> /tmp/debug
+echo 'ps aux | grep mongodb'
+ps aux | grep mongodb >> /tmp/debug
